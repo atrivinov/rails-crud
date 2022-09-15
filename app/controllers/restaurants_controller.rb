7dbff3510cc07b4c25ff8a2d27698eff97b03083
alexandra trivino <alexandratrivino2@gmail.com>
2022-09-15 16:49:44 -0500

    final version

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,13 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
   def index
     @restaurants = Restaurant.all
   end
 
   def show
     # raise
-    @restaurant = Restaurant.find(params[:id])
+    set_restaurant
   end
 
   def new
@@ -24,18 +26,18 @@ class RestaurantsController < ApplicationController
   end
 
   def edit
-    @restaurant = Restaurant.find(params[:id])
+    set_restaurant
   end
 
   def update
-    @restaurant = Restaurant.find(params[:id])
+    set_restaurant
     @restaurant.update(restaurant_params)
     redirect_to restaurant_path(@restaurant) # restaurant_path contains show as route
   end
 
 
   def destroy
-    @restaurant = Restaurant.find(params[:id])
+    set_restaurant
     @restaurant.destroy
     # No need for app/views/restaurants/destroy.html.erb
     redirect_to restaurants_path, status: :see_other # permite ver un status code 303
@@ -46,5 +48,9 @@ class RestaurantsController < ApplicationController
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :address, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get "restaurants/new", to: "restaurants#new"# , as: :new_restaurant # Se debe colocar antes de id
   post "restaurants", to: "restaurants#create"
   get "restaurants/:id", to: "restaurants#show", as: :restaurant
-  get "restaurants/:id/edit", to: "restaurants#edit"# , as: :edit_restaurant
+  get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
   patch "restaurants/:id", to: "restaurants#update"
   delete "restaurants/:id", to: "restaurants#destroy"
 end
